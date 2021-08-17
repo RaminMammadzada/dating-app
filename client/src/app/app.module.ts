77ed1c59100aac5525c11f9586439818d0a5ee8b
Ramin Mammadzada <remki-design@hotmail.com>
2021-08-17 21:07:21 +0400

    Add not-found, server-error and test-errors components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
